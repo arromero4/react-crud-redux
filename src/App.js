4162177f5b51ae4e0d7f6bcccd4327e0fc587aed
arromero4 <arromero0408@gmail.com>
2022-09-04 21:57:44 -0500

    crud redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useSelector } from "react-redux";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const tasksState = useSelector((state) => state.tasks);
@@ -10,14 +10,14 @@ function App() {
   return (
     <div className="bg-zinc-900 h-screen text-white">
       <div className="flex items-center justify-center h-full">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<TaskList />}/>
           <Route path="/create-task" element={<TaskForm />}/>
           <Route path="/edit-task/:id" element={<TaskForm />} />
 
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       </div>
     </div>
   );
